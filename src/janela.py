1c5bfecfd5cae5fbca0c8d812d6d2e6cd297ce81
Guilherme <guilhermerd6@hotmail.com>
2021-08-22 02:25:36 -0300

    Refatorada criação dos checkbuttons

--- src/janela.py
+++ src/janela.py
@@ -14,39 +14,25 @@ class LstmClass:
         self.container1.grid()
         self.textoInicial = Label(self.container1, text="Selecione abaixo as entradas do modelo")
         self.textoInicial.grid(row=0)
-        self.initVariables()
         self.initCheckboxes()
         self.initButton()
 
-    # Definição das variáveis
-    def initVariables(self):
-        self.gameDuration = tk.StringVar()
-        self.firstBlood = tk.StringVar()
-        self.firstTower = tk.StringVar()
-        self.firstInhibitor = tk.StringVar()
-        self.firstBaron = tk.StringVar()
-        self.firstDragon = tk.StringVar()
-        self.firstRiftHerald = tk.StringVar()
-
-    # Checkbuttons com as seleções das variáveis
+    # Checkbuttons com os textos das entradas
     def initCheckboxes(self):
         self.container2 = Frame(self.master)
         self.container2.grid()
-        self.chkGameDuration = tk.Checkbutton(self.container2, text='Tempo de jogo', var=self.gameDuration)
-        self.chkGameDuration.grid(column=0, row=0, padx=10, pady=10)
-        self.chkFirstBlood = tk.Checkbutton(self.container2, text='Primeiro abate', var=self.firstBlood,
-                                            onvalue="FirstBlood", offvalue="")
-        self.chkFirstBlood.grid(column=1, row=0, padx=10, pady=10)
-        self.chkfirstTower = tk.Checkbutton(self.container2, text='Primeira torre', var=self.firstTower)
-        self.chkfirstTower.grid(column=2, row=0, padx=10, pady=10)
-        self.chkfirstInhibitor = tk.Checkbutton(self.container2, text='Primeiro inibidor', var=self.firstInhibitor)
-        self.chkfirstInhibitor.grid(column=3, row=0, padx=10, pady=10)
-        self.chkfirstBaron = tk.Checkbutton(self.container2, text='Primeiro baron', var=self.firstBaron)
-        self.chkfirstBaron.grid(column=0, row=1, padx=10, pady=10)
-        self.chkfirstDragon = tk.Checkbutton(self.container2, text='Primeiro dragão', var=self.firstDragon)
-        self.chkfirstDragon.grid(column=1, row=1, padx=10, pady=10)
-        self.chkfirstRiftHerald = tk.Checkbutton(self.container2, text='Primeiro arauto', var=self.firstRiftHerald)
-        self.chkfirstRiftHerald.grid(column=2, row=1, padx=10, pady=10)
+
+        my_entries = ['gameDuration', 'firstBlood', 'firstTower', 'firstInhibitor', 'firstBaron', 'firstDragon',
+                      'firstRiftHerald']
+        self.check_boxes = []
+
+        for item in my_entries:
+            string_var = tk.StringVar()
+            self.check_boxes.append(string_var)
+
+            cb = tk.Checkbutton(self.container2, text=item, variable=string_var, anchor='w', onvalue=item, offvalue='',
+                                width=50)
+            cb.grid()
 
     # Botão de iniciar modelo
     def initButton(self):
@@ -60,12 +46,17 @@ class LstmClass:
         self.button.grid(row=0)
 
     def plot(self, event):
+        for string_var in self.check_boxes:
+            text = string_var.get()
+            if text:
+                print(text)
+
         f = Figure(figsize=(5, 5), dpi=100)
         a = f.add_subplot(111)
         a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
 
         canvas = FigureCanvasTkAgg(f, self.container3)
-        canvas.get_tk_widget().grid(row=1, pady=10, padx=10)
+        canvas.get_tk_widget().grid(pady=10, padx=10)
         canvas.draw()
 
 
